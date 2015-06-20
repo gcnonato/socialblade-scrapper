ee3db8d1363dfdd9fab7798607e80a232eebf3bf
alrocar <caldodeave@gmail.com>
2015-06-20 22:05:57 +0200

    Added support for arguments

--- topcountry/socialblade-topcountry.js
+++ topcountry/socialblade-topcountry.js
@@ -1,11 +1,11 @@
 var fs = require('fs');
 var request = require("request"),
     cheerio = require("cheerio"),
-    url = "http://socialblade.com/youtube/top/country/ES";
+    url = process.argv[2] || "http://socialblade.com/youtube/top/country/ES";
 
 var replaceAll = function(find, replace, str) {
-  var find = find.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
-  return str.replace(new RegExp(find, 'g'), replace);
+    var find = find.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+    return str.replace(new RegExp(find, 'g'), replace);
 };
 
 request(url, function(error, response, body) {
@@ -15,21 +15,21 @@ request(url, function(error, response, body) {
         var result = [];
 
         $('div.TableMonthlyStats').filter(function(i, el) {
-          if ($(this).find("a[href*=youtube]")) {
-            var text = $(this).find("a[href*=youtube]").text();
-            if (text) {
-              var ch = {};
-              ch.usr = text;
-              ch.channel = 'http://' + $(this).find("a[href*=youtube]").attr('href');
-              ch.subs = replaceAll(',', '', $(this).next('div').find('span').text());
-              ch.views = replaceAll(',', '', $(this).next('div').next('div').find('span').text());
-              result.push(ch);
+            if ($(this).find("a[href*=youtube]")) {
+                var text = $(this).find("a[href*=youtube]").text();
+                if (text) {
+                    var ch = {};
+                    ch.usr = text;
+                    ch.channel = 'http://' + $(this).find("a[href*=youtube]").attr('href');
+                    ch.subs = replaceAll(',', '', $(this).next('div').find('span').text());
+                    ch.views = replaceAll(',', '', $(this).next('div').next('div').find('span').text());
+                    result.push(ch);
+                }
             }
-          }
         });
 
         var es = {
-          channels: result
+            channels: result
         };
 
         fs.writeFile('output.json', JSON.stringify(es, null, 4), function(err) {
