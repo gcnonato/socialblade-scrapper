7bfae49a71cef9ad76869b856d336d380f1c97e7
alrocar <caldodeave@gmail.com>
2015-06-21 12:01:38 +0200

    Exported module

--- topcountry/socialblade-topcountry.js
+++ topcountry/socialblade-topcountry.js
@@ -9,44 +9,52 @@ var replaceAll = function(find, replace, str) {
 };
 
 var getCountryCode = function(url) {
-  if (!url) {
-    return '';
-  }
+    if (!url) {
+        return '';
+    }
 
-  var parts = url.split('/');
-  return parts[parts.length - 1];
+    var parts = url.split('/');
+    return parts[parts.length - 1];
 };
 
-request(url, function(error, response, body) {
-    if (!error) {
-        var $ = cheerio.load(body);
+var topcountry = {};
 
-        var result = [];
+topcountry.request = function(url) {
+    request(url, function(error, response, body) {
+        if (!error) {
+            var $ = cheerio.load(body);
 
-        $('div.TableMonthlyStats').filter(function(i, el) {
-            if ($(this).find("a[href*=youtube]")) {
-                var text = $(this).find("a[href*=youtube]").text();
-                if (text) {
-                    var ch = {};
-                    ch.usr = text;
-                    ch.channel = 'http://' + $(this).find("a[href*=youtube]").attr('href');
-                    ch.subs = replaceAll(',', '', $(this).next('div').find('span').text());
-                    ch.views = replaceAll(',', '', $(this).next('div').next('div').find('span').text());
-                    result.push(ch);
+            var result = [];
+
+            $('div.TableMonthlyStats').filter(function(i, el) {
+                if ($(this).find("a[href*=youtube]")) {
+                    var text = $(this).find("a[href*=youtube]").text();
+                    if (text) {
+                        var ch = {};
+                        ch.usr = text;
+                        ch.channel = 'http://' + $(this).find("a[href*=youtube]").attr('href');
+                        ch.subs = replaceAll(',', '', $(this).next('div').find('span').text());
+                        ch.views = replaceAll(',', '', $(this).next('div').next('div').find('span').text());
+                        result.push(ch);
+                    }
                 }
-            }
-        });
+            });
 
-        var es = {
-            channels: result
-        };
+            var es = {
+                channels: result
+            };
 
-        var output = 'top_channels_' + getCountryCode(url) + '.json';
-        fs.writeFile(output, JSON.stringify(es, null, 4), function(err) {
-            console.log('File successfully written! - Check your project directory for the ' + output + ' file');
-        });
+            var output = 'top_channels_' + getCountryCode(url) + '.json';
+            fs.writeFile(output, JSON.stringify(es, null, 4), function(err) {
+                console.log('File successfully written! - Check your project directory for the ' + output + ' file');
+            });
 
-    } else {
-        console.log("We’ve encountered an error: " + error);
-    }
-});
+        } else {
+            console.log("We’ve encountered an error: " + error);
+        }
+    });
+}
+
+topcountry.request(url);
+
+exports = module.exports = topcountry;
