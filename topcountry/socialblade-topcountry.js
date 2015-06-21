29467bc0e3bf4c854ae8f4715d8096b4cab08eb3
alrocar <caldodeave@gmail.com>
2015-06-21 21:17:14 +0200

    Integrated with Youtube Data API to download complete info of channels

--- topcountry/socialblade-topcountry.js
+++ topcountry/socialblade-topcountry.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
 var request = require("request"),
+    syncRequest = require("sync-request"),
     cheerio = require("cheerio"),
+    sleep = require("sleep"),
     download = process.argv[4],
     topchannel = require("../topchannel/socialblade-topchannel.js"),
     url = ('http://socialblade.com/youtube/top/country/' + process.argv[2]) || "http://socialblade.com/youtube/top/country/ES";
@@ -24,6 +26,7 @@ var getCountryCode = function(url) {
 var topcountry = function() {};
 
 topcountry.prototype.request = function(url) {
+    var self = this;
     request(url, function(error, response, body) {
         if (!error) {
             var $ = cheerio.load(body);
@@ -39,6 +42,15 @@ topcountry.prototype.request = function(url) {
                         ch.channel = 'http://' + $(this).find("a[href*=youtube]").attr('href');
                         ch.subs = replaceAll(',', '', $(this).next('div').find('span').text());
                         ch.views = replaceAll(',', '', $(this).next('div').next('div').find('span').text());
+
+                        try {
+                            sleep.sleep(5);
+                            var channelInfo = self.downloadChannelInfo(text, API_KEY);
+                            ch.channelInfo = channelInfo;
+                        } catch (ignore) {
+                            console.log(ignore);
+                        }
+
                         result.push(ch);
                     }
                 }
@@ -54,7 +66,7 @@ topcountry.prototype.request = function(url) {
             if (!fs.existsSync(countryCode)) {
                 fs.mkdirSync(countryCode);
             }
-            
+
             fs.writeFile(output, JSON.stringify(es, null, 4), function(err) {
                 console.log('File successfully written! - Check your project directory for the ' + output + ' file');
             });
@@ -67,6 +79,19 @@ topcountry.prototype.request = function(url) {
             console.log("We’ve encountered an error: " + error);
         }
     });
+};
+
+topcountry.prototype.downloadChannelInfo = function(channelName, youtubeApiKey) {
+    console.log('downloading channel info: ' + channelName);
+    var urlID = 'https://www.googleapis.com/youtube/v3/channels?key=' + youtubeApiKey + '&forUsername=' + channelName + '&part=id';
+    var res = syncRequest('GET', urlID);
+
+    var channelId = JSON.parse(res.getBody()).items[0].id;
+
+    var urlComplete = 'https://www.googleapis.com/youtube/v3/channels?key=' + youtubeApiKey + '&id=' + channelId + '&part=id,snippet,brandingSettings,contentDetails,invideoPromotion,statistics,topicDetails';
+    res = syncRequest('GET', urlComplete);
+
+    return JSON.parse(res.getBody()).items[0];
 };
 
 topcountry.prototype.download = function(folder, result) {
